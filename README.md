04aa3afddbdcb36bedeb4c845ae656377b1d4005
ngell1 <ngell1@binghamton.eduiuiuiu>
2024-05-02 21:47:04 -0400

    j

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=14691212&assignment_repo_type=AssignmentRepo)
 
-:warning: Everything between << >> needs to be replaced (remove << >> after replacing)
 
-# << Project Title >>
-## CS110 Final Project  << Semester, Year >>
+#  Typing Test 
+## CS110 Final Project   Semester 2, 2024 
 
 ## Team Members
 
@@ -11,7 +10,9 @@ Noah Gell
 
 ## Project Description
 
-A typing game and test. There will be a menu screen allowing users to select the test or the game or to exit. After a test or game is completed, another menu screen Users will haev the ability to modify the test, for example how long the test is, and whether capitals and punctuation will be used. 
+A typing test. The program has a start menu, pause menu, and the test screen. On the test screen their are buttons that allow you to modify whether the text has punctuation or capitlization. The program is incomplete, the only parts that work are switching between game states, tokenizing sentences for the test from a text file using the nltk, and displaying whether user input matches the sentence. 
+
+The test also has "Stats" screen accesible from the main menu, which was supposed to display a users progress by graphing their wpm and accuracy over time. (this data would be stored and accessed from a text file.) Since the test is unfinished, i just wrote the text file in order to display this part. 
 
 ***    
 
@@ -39,12 +40,26 @@ A typing game and test. There will be a menu screen allowing users to select the
 
 ### Classes
 
-- << You should have a list of each of your classes with a description >>
+- buttons: the buttons class creates butons on the screen, and returns when theyve been pressed down. they also change color based on whether the mousing is hovering or theyre clicked. 
+
+- generate_sentences: this class uses the nltk module to tokenize a long text file into sentences, select one at random, and modify it according to modes. 
+
+- startMenu: this class is the start menu game state, it draws a screen with buttons on it, the start button switches the state to "testScreen" and the quit button quits the game. 
+
+- pauseS: this is the pause screen state, draws a screen with buttons, its buttons change to the "testScreen" and "startMenu" state respectively.
+
+- testScreen: this is the main game state. it also draws a screen with buttons. this state is the condition to trigger the eventloop in the controller. 
+
+### Additional modules
+
+- natural language toolkit (https://www.nltk.org/)
 
 ## ATP
 
 | Step                 |Procedure             |Expected Results                   |
 |----------------------|:--------------------:|----------------------------------:|
-|  1                   | Run Counter Program  |GUI window appears with count = 0  |
+|  1                  | 
+
+                                              |GUI window appears with count = 0  |
 |  2                   | click count button   | display changes to count = 1      |
 etc...
--- controller.py
+++ controller.py
@@ -1,54 +1,154 @@
 import pygame
-from classes import buttons
+from classes import buttons, gamestate, testScreen, startMenu, pauseS 
+import nltk
+import string
+from nltk.tokenize import sent_tokenize
+from nltk import word_tokenize
+import random
 width = 800 
 height = 500
 timer = pygame.time.Clock()
+typed = 'sample text'
+typecomplete = ''
+begin = True
 paused = False
+char = ['a','b','c','d','e','f','g','h','i','j','k','l',
+'m','n','o','p','q','r','s','t','u','v','w','x','y','z','A',
+'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+'Q','R','S','T','U','V','W','X','Y','Z','.',',',':',';','!']
 
 
+def sentence_generator(punc,cap,surface):
+    with open('maninthehighcastle.txt') as file:
+        raw_text = file.read()
+    list = sent_tokenize(raw_text)
+    sorted_list = []
+    for i in list:
+        if len(i) > 95 and len(i) > 125:
+            sorted_list.append(i)
+    #lenfth = 54
+
+    sent = random.choice(sorted_list)
+    if punc == False:
+        remove_punct = str.maketrans("", "", string.punctuation)
+        sent = sent.translate(remove_punct)
+    if cap == False:
+        sent == sent.lower()
+
+
+    sent = sent.translate(remove_punct)
+    dashremove = sent.split("â€”")
+    sent = " ".join(dashremove)
+    
+    wordlist = word_tokenize(sent)
+    sent1 = " ".join(wordlist[:10])
+    sent2 =" ".join(wordlist[11:20])
+    sent3 = " ".join(wordlist[21:31])
+
+    
+    font = pygame.font.SysFont('mspgothic', 16)
+
+    full = [sent1,sent2,sent3]
+    #full is what it should be 
+
+    newlines = []
+    
+    for i in full:
+        xvar = 54
+        yvar = 180+((full.index(i))*30) 
+        line = i
+        newline = screenline(i, xvar, yvar, surface)
+        # i is what it should be (a sentence)
+
+
+        newlines.append(newline)
+        #current tasks: make newlines a not a NoneType object!!!
+        #so, we must go to screenline and fix this issue 
+    print(f'newlines:{newlines}')
+        
+
+    return newlines
+
+class screenline:#Word
+    def __init__(self, line, xvar, yvar, surface):
+        self.line = str(line)
+        self.yvar = yvar
+        self.xvar = xvar
+
+        self.surface = surface
+        #it only sets values for these four arguments! when saved, and used with lineblit(), it should work
+ 
+    def lineblit(self):
+        self.font = pygame.font.SysFont('mspgothic', 16)
+        xvar = 54
+        yvar = 180 
+        text =self.font.render(self.line,True, "white" )
+        self.surface.blit(text, (xvar, yvar ))
+        len_typed = len(typed)
+        if typed == self.line[:len_typed]:
+            text = self.font.render(typed,True, "green" )
+            self.surface.blit(text, (xvar, yvar))
+    #screenline doesnt return anything!
+
+        
 class controller:
+    
     def __init__(self):
+        pygame.init()
         self.screen = pygame.display.set_mode((width, height))
         self.backround = pygame.Surface((width,height))
         self.timer = pygame.time.Clock()
         self.fps = 60
-        #pygame.draw.rect can provide a rectangle on which letters can go semi transparently, draw lines, screen.blit(for text)
         pygame.display.set_caption('Typing Test')
         self.screen.fill('black')
         self.timer.tick(self.fps)
 
-        #draw screen details, call button functions for buttons 
-   
+        self.gamestate = gamestate('startMenu')
+        self.testScreen = testScreen(self.screen, self.gamestate)
+        self.startMenu = startMenu(self.screen, self.gamestate)
+        self.pauseS = pauseS(self.screen, self.gamestate)
 
-
-
-
+        self.states = {'testScreen':self.testScreen, 'startMenu':self.startMenu, 'pauseS':self.pauseS}
+        
     def mainloop(self):
-        def drawscreen(self):
-            timer.tick(60)
-            pygame.init()
-            pygame.draw.rect(self.screen,"darkblue",(650, 0, 150,height) )
-            pygame.draw.rect(self.screen,"grey",(650, 0, 150,height),2)
-            pygame.draw.rect(self.screen,"darkblue",(0, 400, width,100) )
-            pygame.draw.rect(self.screen,"grey",(0, 400, width,100),2 )
-            font = pygame.font.SysFont('mspgothic', 16)
-            #__init__x, y, type, font, text, state, surface
-            button1 = buttons(675, 20,"rect_1", font, "test button", "pressed", self.screen)
-            button1.draw()
+        begin = True
+        paused = False
+        typed = ''
+        typecomplete = ''
+        queuelevel = True
+
+
 
         run = True
         while run:
             
-            status = drawscreen(self)
+            if paused:
+                pass
+                #self.gamestate.change('pauseS') #right version of gamestate?
+            # elif que_next:
+            #     sentences = sentences.getsent(self.state, False, False, self.screen,typed)
+            #     que_next = False
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-                if event.type == pygame.KEYDOWN:
-                    if not paused:
-                        
+
+            L = self.states[self.gamestate.check()].run()
+            if L == "quit":
+                pygame.quit()
+            if L: #probably wrong
+                paused = True
+                pass
+
 
             pygame.display.flip()
         pygame.quit()
+
+
+
+
+
+
+        
 
 
 
